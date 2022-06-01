4490bd0593ed0a98d730026cb149552b959b6dec
Marceometry <marceometry@gmail.com>
2022-06-01 12:00:00 -0300

    feat: componentize project

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react'
+import { GenreResponseProps } from '../App'
+import { api } from '../services/api'
+import { Button } from './Button'
+
+type SideProps = {
+  selectedGenreId: number
+  setSelectedGenreId: (id: number) => void
+}
+
+export function SideBar({ selectedGenreId, setSelectedGenreId }: SideProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([])
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data)
+    })
+  }, [])
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id)
+  }
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
